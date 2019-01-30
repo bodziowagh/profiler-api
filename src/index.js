796e28c9d44c8fa40c68ca7dc6262c6edfa1ea6c
bodziowagh <k.wolinski@univapay.com>
2019-01-30 18:43:47 +0900

    Configurable host and error handling

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@
 import http from "http";
 import "dotenv/config";
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(`Sending test var: ${ process.env.TEST_ENV_VAR }`);
+try {
+	console.log("Initializing...");
 
-		console.log("Respond has been sent sir!");
-}).listen(8080);
+	const host = process.env.HOST;	// TODO: handle null
+
+	if (!host) {
+		throw "Host has not been defined!";
+	}
+
+	http.createServer(function (req, res) {
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(`Sending test var: ${ process.env.TEST_ENV_VAR }`);
+
+			console.log("Respond has been sent sir!");
+	}).listen(host);
+
+	console.log(`SUCCESS: Listening at ${host}`);
+} catch (error) {
+	console.log("FAILURE: An error occured while starting the server: ", error);
+
+	throw error;
+}
